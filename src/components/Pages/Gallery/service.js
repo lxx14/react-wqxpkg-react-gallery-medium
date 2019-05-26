44b410279c0a6b471594b2583201ab1a644fea57
lxx14 <lxx1427@gmail.com>
2019-05-26 14:38:56 +0300

    fixed bugs

--- src/components/Pages/Gallery/service.js
+++ src/components/Pages/Gallery/service.js
@@ -1,7 +1,7 @@
 const client_id = `ca5a2a324ba06f2cf8bede88a989bb6c2f5f87730032b3c6256b72888f2cc94c`; //ключ клиента
 const base_url = `https://api.unsplash.com/`; //неизменная часть адреса, у которого спрашиваем
 
-export const getPhotos = (page=1, per_page=10) => { 
+export const getPhotos = (page=2, per_page=20) => { 
 
   // per_page  в данном случае-количество элементов (все это в документации можно посмотреть). Передаем в параметры функции
   // page   какая страница с фотками(тоже из документации)
